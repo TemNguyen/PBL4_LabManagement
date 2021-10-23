78353ca69b61788f618cad37784a929affbd3cfa
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2021-10-23 08:49:39 +0700

    init client's service pj

---
+++ PC_Heal_ClientService/PC_Heal_ClientService/ProjectInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Heal_ClientService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
