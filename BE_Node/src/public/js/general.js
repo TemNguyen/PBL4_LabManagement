1fd780bbeecb1cc228cd0a8460409f8e5428f125
VoVanThanh <thanhvo618@gmail.com>
2021-10-25 20:54:02 +0700

    delete folder BE_Node in FE

--- BE_Node/src/public/js/general.js
+++
@@ -1,9 +0,0 @@
-const sidebarItems = document.querySelectorAll('.sidebar-item');
-sidebarItems.forEach(sidebarItem => {
-    sidebarItem.onclick = (e) => {
-        e.preventDefault();
-        const activeItem = document.querySelector('.sidebar-item.active');
-        activeItem.classList.remove('active');
-        sidebarItem.classList.add('active');
-    }
-});
--- BE_Node/src/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const loginController = require('../app/controllers/LoginController');
-
-router.get('/', loginController.index);
-
-module.exports = router;
