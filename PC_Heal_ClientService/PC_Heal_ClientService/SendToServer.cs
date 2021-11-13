0db5b9416c61687b820e22a76cc7dc06a80906c2
Nguyễn Duy Thịnh <ndthinhdut@outlook.com>
2021-11-13 17:41:54 +0700

    add env ServerIP and Serverport

--- PC_Heal_ClientService/PC_Heal_ClientService/SendToServer.cs
+++ PC_Heal_ClientService/PC_Heal_ClientService/SendToServer.cs
@@ -17,8 +17,36 @@ namespace PC_Heal_ClientService
 
         public static async Task Send(CI computerInfo)
         {
-            var serverIp = IPAddress.Parse("192.168.0.104");
-            var serverPort = 5000;
+            IPAddress serverIp = null;
+            int serverPort = 0;
+
+            string rootPath = AppDomain.CurrentDomain.BaseDirectory.ToString();
+            string confPath = rootPath + @"service.conf";
+
+            if (File.Exists(confPath))
+            {
+                var sr = new StreamReader(confPath);
+                var line = await sr.ReadToEndAsync();
+                string[] data = line.Split(new char[]
+                {
+                    '\r', '\n'
+                });
+                serverIp = IPAddress.Parse(data[0]);
+                serverPort = Convert.ToInt32(data[2]);
+                sr.Close();
+            }
+            else
+            {
+                //create new file
+                using (StreamWriter sw = File.CreateText(confPath))
+                {
+                    await sw.WriteAsync("192.168.0.8\r\n5000");
+                }
+                //use default ip and port
+                serverIp = IPAddress.Parse("192.168.0.8");
+                serverPort = 5000;
+            }
+
 
             TcpClient client = new TcpClient();
             await client.ConnectAsync(serverIp, serverPort);
